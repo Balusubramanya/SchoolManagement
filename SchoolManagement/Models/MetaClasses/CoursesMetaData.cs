ff5f36b5f106ffbf214340a943343b037ae6a3cb
Prashanth <prashanthitdf@outlook.com>
2021-06-15 16:03:34 +0530

    Add project files.

---
+++ SchoolManagement/Models/MetaClasses/CoursesMetaData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.Models
+{
+    public class CoursesMetaData
+    {
+        [StringLength(50)]
+        [Display (Name ="Course Titles")]
+        public string Title { get; set; }
+        [Range(1,8)]
+        [Display(Name = "Course Credits")]
+        public int Credits { get; set; }
+    }
+    //the below partial class is created because of to access the data of entity model classes validations with user defined class ex.CoursesMetaData class
+    //for Courses entity model classes
+    [MetadataType(typeof(CoursesMetaData))]
+    public partial class Course
+    {
+
+    }
+}
---
+++ SchoolManagement/Models/MetaClasses/StudentsMetaData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.Models
+{
+    public class StudentsMetaData
+    {
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Enrollment Date")]
+        public Nullable<System.DateTime> EnrollmentDate { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+        [Display(Name = "Date Of Birth")]
+        public Nullable<System.DateTime> DateOfBirth { get; set; }
+    }
+    //the below partial class is created because of to access the data of entity model classes validations with user defined class ex.StudentsMetaData class
+    //for Students entity model classes
+    [MetadataType(typeof(StudentsMetaData))]
+    public partial class Student { }
+}
